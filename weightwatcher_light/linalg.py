c30693c28b1c96061a1e1736fcf50b3ca6e19dab
Whadup <lukas.pfahler@udo.edu>
2022-06-09 14:42:50 +0200

    seems to do the job quite well

--- weightwatcher_light/linalg.py
+++ weightwatcher_light/linalg.py
@@ -38,6 +38,7 @@ class AlphaEstimator():
         if x_min is None:
             self.coeffs = torch.zeros(4096, deg).to(device)
             self.x_min = np.logspace(-8, np.log10(x_max), self.coeffs.shape[0] // 2)
+            # self.x_min = np.linspace(1e-8, x_max, self.coeffs.shape[0] // 2)
         else:
             self.coeffs = torch.zeros(2, deg).to(device)
             self.x_min = np.array([x_min])
@@ -48,18 +49,18 @@ class AlphaEstimator():
             return 1.0 * (((x + 1) * (0.5 * x_max)) >= x_min)
         for i, x_min in enumerate(self.x_min):
             self.coeffs[2 * i] = torch.tensor(np.polynomial.chebyshev.chebinterpolate(func, deg - 1, args=(x_min,))).to(device)
-            self.coeffs[2 * i + 1] = torch.tensor(np.polynomial.chebyshev.chebinterpolate(func2, deg - 1, args=(x_min,))).to(device)
-            # alphaP  =  np.pi / (self.deg + 2)
-            # # print(x_min, x_max)
-            # a = min(x_min / (0.5 * x_max) - 1, 1)
-            # b = 1
-            # # print(a, b)
-            # self.coeffs[2 * i + 1, 0] = 1.0 / pi * (acos(a) - acos(b)) * \
-            #              (sin(1) * alphaP / ((self.deg + 2)*sin(alphaP)) + (1-(1)/(self.deg + 2)) * cos(0 * alphaP))
-            # for j in range(1, deg):
-            #     self.coeffs[2 * i + 1, j] = 2.0 / pi * (sin(j * acos(a)) - sin(j * acos(b))) / j * \
-            #         (sin(j + 1)* alphaP / ((self.deg + 2) * sin(alphaP)) + (1 - (j + 1) / (self.deg + 2)) * cos(j * alphaP))
-            tmp = np.linspace(-1,1,10000)
+
+            # self.coeffs[2 * i + 1] = torch.tensor(np.polynomial.chebyshev.chebinterpolate(func2, deg - 1, args=(x_min,))).to(device)
+        # Closed form Solution for Step Function
+        alphaP  =  np.pi / (self.deg + 2)
+        a = np.minimum(self.x_min / (0.5 * x_max) - 1, 1)
+        b = 1
+        self.coeffs[1::2, 0] = torch.tensor(1.0 / pi * (np.arccos(a) - np.arccos(b)))# * \
+        # self.coeffs[1::2, 0] *= torch.tensor((sin(1) * alphaP / ((self.deg + 2)*sin(alphaP)) + (1-(1)/(self.deg + 2)) * cos(0 * alphaP)))
+        for j in range(1, deg):
+            self.coeffs[1::2, j] = torch.tensor(2.0 / pi * (np.sin(j * np.arccos(a)) - np.sin(j * np.arccos(b))) / j)
+            # self.coeffs[1::2, j] *= torch.tensor((np.sin(j + 1)* alphaP / ((self.deg + 2) * np.sin(alphaP)) + (1 - (j + 1) / (self.deg + 2)) * np.cos(j * alphaP)))
+            # tmp = np.linspace(-1,1,10000)
             # print(np.sqrt(np.mean((np.polynomial.chebyshev.chebval(tmp, self.coeffs[2 * i + 1].cpu().numpy()) - func2(tmp, x_min))**2)))
         self.samples = 1024
         self.X = None
@@ -74,50 +75,65 @@ class AlphaEstimator():
             yy = torch.nn.functional.linear(y, self.layer.weight.T, None)
             y = torch.nn.functional.linear(yy, self.layer.weight, None)
         return (2.0 / self.x_max) * y.view(-1, self.m).detach() - W #  * x - y
-
+    def matMatT(self, W):
+        # y = torch.as_tensor(w, device=self.device).float().view(self.shape_out)
+        yy = W.view(-1, *self.shape_in[1:])
+        if isinstance(self.layer, torch.nn.Conv2d):
+            y = torch.nn.functional.conv2d(yy, self.layer.weight, None, self.layer.stride, self.layer.padding, self.layer.dilation, self.layer.groups)
+            tmp = (torch.tensor(self.shape_in[2:3]) - 1) * torch.tensor(self.layer.stride) - 2 * torch.tensor(self.layer.padding) + torch.tensor(self.layer.dilation) * (torch.tensor(self.layer.weight.shape[2:3]) -1 ) + 1
+            output_padding = torch.tensor(self.shape_in[2:3]) - tmp
+            yy = torch.nn.functional.conv_transpose2d(y, self.layer.weight, None, self.layer.stride, self.layer.padding, tuple(output_padding), self.layer.groups, self.layer.dilation)
+        elif isinstance(self.layer, torch.nn.Linear):
+            y = torch.nn.functional.linear(yy, self.layer.weight, None)
+            yy = torch.nn.functional.linear(y, self.layer.weight.T, None)
+        return (2.0 / self.x_max) * yy.view(-1, self.n).detach() - W 
     def normalSample(self):
         self.X = torch.nn.init.normal_(self.X)
 
     @torch.no_grad()
     def produceSample(self):
-        self.X = torch.zeros((self.samples, self.m)).to(self.device).detach()
+        m = min(self.m, self.n)
+        if m == self.m:
+            matMat = self.matMat
+        else:
+            matMat = self.matMatT
+        self.X = torch.zeros((self.samples, m)).to(self.device).detach()
         self.normalSample()
         # M2 = torch.zeros((self.samples, self.m)).to(self.device).detach()
         # M2 = M2.new_tensor(self.X).to(self.device).detach()
         M2 = self.X.clone().detach()
-        M1 = self.matMat(M2)
+        M1 = matMat(M2)
         # print(torch.linalg.norm(self.X, dim=1).mean())
         # print(torch.linalg.norm(M2, dim=1).mean())
         # print(torch.linalg.norm(M1, dim=1).mean())
 
         # f = torch.zeros((self.samples, 1)).to(self.device).detach()
-        f = (self.coeffs[:, 0] * torch.bmm(self.X.view(self.samples, 1, self.m), M2.view(self.samples, self.m, 1)) +
-             self.coeffs[:, 1] * torch.bmm(self.X.view(self.samples, 1, self.m), M1.view(self.samples, self.m, 1)))
+        f = (self.coeffs[:, 0] * torch.bmm(self.X.view(self.samples, 1, m), M2.view(self.samples, m, 1)) +
+             self.coeffs[:, 1] * torch.bmm(self.X.view(self.samples, 1, m), M1.view(self.samples, m, 1)))
 
         for i in range(2, self.deg):
-            F = 2.0 * self.matMat(M1) - M2
+            F = 2.0 * matMat(M1) - M2
             # print(torch.linalg.norm(F, dim=1).mean())
 
             del M2
             M2 = M1
             M1 = F
             # print(M2.shape,M1.shape,F.shape,torch.norm(F,dim=1))
-            f  = f + self.coeffs[:, i] * torch.bmm(self.X.view(self.samples, 1, self.m), F.view(self.samples, self.m, 1))
+            f  = f + self.coeffs[:, i] * torch.bmm(self.X.view(self.samples, 1, m), F.view(self.samples, m, 1))
             # if len(self.coeffs) == 1:
             #     print("f_"+str(i), torch.mean(f, dim=(0, 1)))
         return f#torch.matmul(torch.t(self.X),f)
 
-    def estimateAlpha(self):
+    def estimateAlpha(self, draws=1):
         t = torch.zeros(self.coeffs.shape[0]).to(self.device)
-        draws = 16
         for repeats in range(draws):
             particles = self.produceSample()
             t += particles.mean(dim=(0, 1))
             # print(particles.std(dim=(0,1)) / particles.mean(dim=(0,1)))
         t /= 1.0 * draws
         return 1 + t[1::2] / t[::2], t[0::2], t[1::2]
-    def fit(self):
-        alphas, summe, n = self.estimateAlpha()
+    def fit(self, draws=1):
+        alphas, summe, n = self.estimateAlpha(draws=draws)
         # print(n)
         npn = n.cpu().numpy()
         try:
@@ -125,7 +141,27 @@ class AlphaEstimator():
         except:
             which = 0
         # print(n)
-        # x_min = torch.tensor(self.x_min).to(self.device)
+        # print(n[0], empirical_cumulative)
+        x_min = torch.tensor(self.x_min).to(self.device)
+        D_best = 1
+        alpha_best = -1
+        xmin_best = 0
+        n = n.clamp(0, min(self.n, self.m))
+        ignore = len(alphas) // 100 + 1 #TODO: This feels hacky...
+        for i, (xm, alpha) in enumerate(zip(self.x_min[:-ignore], alphas[:-ignore])):
+            empirical_cumulative = (n[i] - n) / n[i]
+            model = 1 - (xm / x_min) ** (alpha - 1) #evaluate at the xmin points
+            D = (model[i+1:] - empirical_cumulative[i+1:]).abs().max()
+            # print((model[i:] - empirical_cumulative[i:]).abs().cpu().numpy(), D.item())
+            # print(alpha, xm, D, ((model[i:] - empirical_cumulative[i:])**2).mean())
+            if D < D_best:
+                xmin_best = xm
+                alpha_best = alpha
+                D_best = D
+                which = i
+            # print(D)
+        
+        # print(n)
         # likelihoods = n * torch.log(alphas - 1) \
         #             + n * (alphas - 1) * torch.log(x_min) \
         #             - summe * (alphas)
@@ -135,10 +171,13 @@ class AlphaEstimator():
         # which = likelihoods.argmax()
         # print(alphas[].item(), self.x_min[likelihoods.argmax()])
         # print("alpha", alphas[which].item(), "\txmin", self.x_min[which],)#, likelihoods[which])
-        self.xmin = self.x_min[which]
+        # self.xmin = self.x_min[which]
+        self.xmin = xmin_best
+        self.D = D_best
+        
         self.summe = summe[which]
         self.est_n = n[which]
-        return alphas[which]
+        return alpha_best
 
 class TopEigValsEstimator():
     def __init__(self,layer, device,shape_in,shape_out):
@@ -173,11 +212,17 @@ class TopEigValsEstimator():
             y = torch.nn.functional.linear(yy, self.layer.weight, None)
         return y.view(self.m, 1).detach()
     def matVecT(self,y):
-        # yy = torch.as_tensor(w, device=self.device, dtype=torch.float32).view(self.shape_out)
-        y = y.view(self.shape_in)
-        yy = torch.nn.functional.conv2d(y, self.layer.weight, None, self.layer.stride, self.layer.padding, self.layer.dilation, self.layer.groups)
-        x = torch.nn.functional.conv_transpose2d(yy, self.layer.weight, None, self.layer.stride, self.layer.padding, 0, self.layer.groups, self.layer.dilation)
-        x = x.view(self.n, 1).detach()#
+
+        yy = y.view(self.shape_in)
+        if isinstance(self.layer, torch.nn.Conv2d):
+            y = torch.nn.functional.conv2d(yy, self.layer.weight, None, self.layer.stride, self.layer.padding, self.layer.dilation, self.layer.groups)
+            tmp = (torch.tensor(self.shape_in[2:3]) - 1) * torch.tensor(self.layer.stride) - 2 * torch.tensor(self.layer.padding) + torch.tensor(self.layer.dilation) * (torch.tensor(self.layer.weight.shape[2:3]) -1 ) + 1
+            output_padding = torch.tensor(self.shape_in[2:3]) - tmp
+            yy = torch.nn.functional.conv_transpose2d(y, self.layer.weight, None, self.layer.stride, self.layer.padding, tuple(output_padding), self.layer.groups, self.layer.dilation)
+        elif isinstance(self.layer, torch.nn.Linear):
+            y = torch.nn.functional.linear(yy, self.layer.weight, None)
+            yy = torch.nn.functional.linear(y, self.layer.weight.T, None)
+        x = yy.view(self.n, 1).detach()#
         # x = cp.array(x)
         return x
         # yy = torch.from_numpy(y.astype(np.float32)).view(self.shape_in).to(self.device)
@@ -193,7 +238,7 @@ class TopEigValsEstimator():
     def estimateEigVals(self,budget):
         #self.X = torch.autograd.Variable(torch.randn(self.shape_in).to(self.device),requires_grad=True)
         #self.H = self.layer(self.X)
-        if True or self.m < self.n:
+        if self.m < self.n:
             s = own_eigsh(self.matVec, self.m, k=min(self.m - 1, budget))
             # s = eigsh(
             #     LinearOperator((self.m,self.m), matvec=self.matVec),
@@ -208,8 +253,7 @@ class TopEigValsEstimator():
             #     maxiter=1000
             # )
         else:
-            WERENEVERHERE
-            s = own_eigsh(self.matVecT, self.n, k=min(self.m - 1, budget))
+            s = own_eigsh(self.matVecT, self.n, k=min(self.n - 1, budget))
             # s = eigsh(
                 # LinearOperator((self.n, self.n), matvec=self.matVecT),
                 # k=min(self.n - 1,budget),
--- weightwatcher_light/weightwatcher.py
+++ weightwatcher_light/weightwatcher.py
@@ -2,39 +2,63 @@ import torch
 import numpy as np
 from torchinfo import summary
 from .linalg import TopEigValsEstimator, AlphaEstimator
+import powerlaw
 
-
-def weightwatcher(model, input_shape, device="cuda", chebyshev_degree=256, num_batches=8, batch_size=1024):
+def weightwatcher(model, input_shape, device="cuda", chebyshev_degree=64, num_batches=1, batch_size=4 * 4096, verbose=True):
     summ = summary(model, input_shape, col_names=("input_size", "output_size"), verbose=0)
     layerwise_info = []
     for l in summ.summary_list:
         input_size = l.input_size
         output_size = l.output_size
-        print(l)
         l = l.module
         if not isinstance(l, (torch.nn.Linear, torch.nn.Conv2d)):
             continue
+        if verbose:
+            print("Analyzing", l, "as ", f"{np.prod(output_size)}x{np.prod(input_size)}", "linear layer")
         
         s = TopEigValsEstimator(l, device, input_size, output_size)
         spectral_norm = s.estimateEigVals(1).item()
+        if verbose:
+            print("Spectral Norm is", spectral_norm)
 
-        A = AlohaEstimator(l, device, input_size, output_size, None, spectral_norm, chebyshev_degree)
-        alpha = A.fit()
+        A = AlphaEstimator(l, device, input_size, output_size, None, spectral_norm, chebyshev_degree)
+        A.samples = batch_size
+        alpha = A.fit(draws=num_batches).item()
+        if verbose:
+            print("Alpha  is", alpha, "with x_min", A.xmin, f"(D={A.D})")
+            if isinstance(l, torch.nn.Linear):
+                s = torch.linalg.svdvals(l.weight).detach().cpu().numpy()  ** 2
+                # print(s)
+                P = powerlaw.Fit(s, suppress_output=True)
+                print("svd + powerlaw produces alpha ", P.alpha, "with x_min", P.xmin, f"(D={P.D})")
+                print("n", A.est_n.item(), len(s[s > A.xmin]) )
         layerwise_info.append(dict(
             layer=l,
             spectral_norm=spectral_norm,
             alpha=alpha,
             weighted_alpha=np.log10(spectral_norm) * alpha,
+            D=A.D.item(),
             input_size=input_size,
             output_size=output_size,
             N=A.n,
             M=A.m,
         ))
-    model_info = { k:np.mean([l[k] for l in layerwise_info]) for k in layerwise_info[0].keys() if key!="layer"}
+    model_info = { k:np.mean([l[k] for l in layerwise_info]) for k in ["spectral_norm", "alpha", "weighted_alpha", "D"]}
     return dict(layers=layerwise_info, **model_info)
 
 
 if __name__ == "__main__":
-    from torchvision.models import resnet18, vgg16
-    model = vgg16(pretrained=True).to("cuda")
-    print(weightwatcher(model, (1, 3, 64, 64)))
+    import torchvision.models as models
+    results = []
+    for model_cls in [models.vgg11_bn, models.vgg13_bn, models.vgg16_bn, models.vgg19_bn]:
+        model = model_cls(pretrained=True).cuda()
+        statistics = weightwatcher(model, (1, 3, 32, 32))
+        statistics.pop("layers")
+        print(statistics)
+        results.append(statistics)
+    
+    for n, r in zip(["VGG11", "VGG13", "VGG16", "VGG19"], results):
+        print(n, r)
+    #VGG13: {'spectral_norm': 81.13524187528171, 'alpha': 2.0631472881023702, 'weighted_alpha': 3.737522625643188}
+    #VGG16: {'spectral_norm': 65.65331041812897, 'alpha': 2.0158966332674026, 'weighted_alpha': 3.43908260343017}
+    #VGG19: {'spectral_norm': 53.92422615854364, 'alpha': 2.144058164797331, 'weighted_alpha': 3.489194736805615}
